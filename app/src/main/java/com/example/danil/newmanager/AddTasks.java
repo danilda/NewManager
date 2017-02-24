2a7ee7c2bc7bd982490d3bf7a43ea41cbe52c180
dach1016 <danil1996-96@mail.ru>
2017-02-24 17:38:41 +0200

    add ArrayAdapter

--- app/src/main/java/com/example/danil/newmanager/AddTasks.java
+++ app/src/main/java/com/example/danil/newmanager/AddTasks.java
@@ -3,6 +3,7 @@ package com.example.danil.newmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class AddTasks extends AppCompatActivity {
     SwitchCompat important;
     SwitchCompat repeated;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +26,17 @@ public class AddTasks extends AppCompatActivity {
         taskClass = (Spinner) findViewById(R.id.task_class);
         important = (SwitchCompat) findViewById(R.id.important);
         repeated = (SwitchCompat) findViewById(R.id.repeated);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        taskClass.setAdapter(adapter);
     }
 
     // TODO добавить классы задач (ДР, покупки, ежедневное, спорт , встреча)
     public boolean validation(){
-
+        throw new RuntimeException();
     }
 
 }
